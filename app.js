d365c8e2fdbff144d9fe6efb65c4c640e200ca63
samuel <acquahsamuel@gmail.com>
2020-05-11 01:29:01 +0000

    handlebars -> config

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
+ 
 const app = express();
 
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs({defaultLayout : 'home'}));
+app.set('view engine', 'handlebars');
 
 
 
+app.get('/' , (req, res) =>{
+    res.render('home/index');
+});
 
-app.listen(4500, ()=>{
-    console.log(`Listening on port 4500`);
+
+app.get('/about' , (req, res) =>{
+    res.render('home/index');
+});
+
+
+app.listen(4550, ()=>{
+    console.log(`Listening on port 4550`);
 })
 
 // const port  = process.env.PORT = 4080;
