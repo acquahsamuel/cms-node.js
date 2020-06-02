fd6c166f237f60da50ee670dac7e4e327142e996
samuel <acquahsamuel@gmail.com>
2020-06-02 16:02:54 +0000

    Login feature

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+
 app.use(express.static(path.join(__dirname, "public")));
 app.engine(
   "handlebars",
