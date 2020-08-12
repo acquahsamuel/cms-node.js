0f140477f0fa6e1ee63e61cd723c58b6bcae68fe
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-12 18:53:54 +0000

    footer update

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const faker = require("faker");
 const router = express.Router();
 const Post = require("../../models/Post");
 const Comment = require("../../models/Comment");
